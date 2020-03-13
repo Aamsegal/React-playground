9d4f4e302f1e125c2cb51552381bf6e5814d82d2
Aaron <amsegal93@gmail.com>
2020-03-13 12:50:29 -0400

    Small commit just to push everything to github while i fix my computer >:(

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Split from './composition/Split';
 import './App.css';
 import Tooltip from './composition/Tooltip';
+import TheDate from './state/TheDate'
+import Counter from './state/Counter'
 
 // make 2 tooltips here and another inside the App directly
-const firstTooltip = (
+/*const firstTooltip = (
   <Tooltip message='tooltip message' color = 'hotpink'>
    ipsum
   </Tooltip>
@@ -20,7 +22,9 @@ function App() {
     <main className='App'>
       <Split className='left' flexBasis='2'>
         This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
+
+        {make another tooltip directly inside the App}
+
         <Tooltip message='one more tooltip message'>
           Necessitatibus?
         </Tooltip>
@@ -29,6 +33,16 @@ function App() {
         This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
       </Split>
     </main>
+  )
+}*/
+
+
+function App() {
+  return (
+    <div>
+      <TheDate />
+      <Counter count={123}/>
+    </div>
   )
 }
 
